72b3f3fd7edc3322844a6707e97c2d4295982c99
oluwaseun bello <bellomsean@gmail.com>
2020-10-19 13:11:12 +0100

    added mario and mushroom icon

---
+++ src/MarioMushroom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import mushroom from '../assets/img/mushroom.jpeg'
+
+const MarioMushroom = () => {
+  return (
+    <img className="mushroom" src={mushroom} alt="mushroom" />
+  )
+}
+
+export default MarioMushroom
---
+++ src/component/Mario.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import mario from '../assets/img/mario.jpeg'
+
+const Mario = () => {
+  return (
+    <img className="mario" src={mario} alt="mario"/>
+  )
+}
+
+export default Mario
