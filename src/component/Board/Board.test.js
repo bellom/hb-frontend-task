0a88d358050a15cd7cfa1ba46c5b429ec315eea7
oluwaseun bello <bellomsean@gmail.com>
2020-10-19 10:51:36 +0100

    added board test

---
+++ src/component/Board/Board.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Board from './Board';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  const props = {
+    cells: Array(9),
+    cols: 3,
+    rows: 3
+  };
+
+  ReactDOM.render(<Board cols={props.cols} rows={props.rows} cells={props.cells} />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
